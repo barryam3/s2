0f56ad641e7ddfee064b813cce0c0dfa1b204cc7
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-12-19 13:06:09 -0500

    Updated typescript to support string enums

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { filterInPlace } from '../utils';
 
 export enum Context {
-  Success,
-  Info,
-  Warning,
-  Danger
+  Success='success',
+  Info='info',
+  Warning='warning',
+  Danger='danger'
 }
 
 export interface Message {
