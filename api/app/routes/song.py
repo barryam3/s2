cca86ca483493d1ac212188716b7597ae2e49f4b
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-07-11 00:24:05 -0400

    Fix song filtering bug

--- api/app/routes/song.py
+++ api/app/routes/song.py
@@ -9,5 +9,6 @@ song = Blueprint('song', __name__)
 @song.route('/', methods=['GET'])
 @login_required
 def get_songs():
-    songs = [s.to_dict() for s in Song.query.filter(**request.args).all()]
+    query_results = Song.query.filter_by(**request.args.to_dict()).all()
+    songs = [s.to_dict() for s in query_results]
     return send_success_response(songs)
