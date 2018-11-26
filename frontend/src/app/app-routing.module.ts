9505ba4ca8bc315097ff218ebba6b31007308b8f
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-11-25 21:07:47 -0500

    Moved setlists to separate page.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ import { UsersComponent } from './users/users.component';
 import { AccountComponent } from './account/account.component';
 import { AppComponent } from './app.component';
 import { AddSongComponent } from './add-song/add-song.component';
+import { ManageSetlistComponent } from './manage-setlist/manage-setlist.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'songs', component: SongsComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'setlists', component: ManageSetlistComponent },
   { path: 'account', component: AccountComponent },
   { path: 'add-song', component: AddSongComponent },
   { path: '', component: AppComponent },
